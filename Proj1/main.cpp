898f838f2b8e8b082e843b75dae0b385a140a5c8
James Curley <jc3181@columbia.edu>
2016-01-10 17:29:07 -0500

    add proj1 test

---
+++ Proj1/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream> //Input-Output
+using namespace std;
+
+int main()
+{
+
+cout << "Hey there" << endl;
+int age;
+cout << "How old are you?";
+cin >> age;
+cout << "You are " << age << " years old" << endl;
+
+    return 0;
+}
